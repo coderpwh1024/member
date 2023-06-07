586e9a1e115f3e3fc75d67ad4633cd4a5345b7d7
coderpwh <pwhfbc@outlook.com>
2023-06-07 13:59:03 +0800

    feat:add MemberPackageOrderDTOassembler

---
+++ src/main/java/com/coderpwh/member/application/assembler/domain/MemberPackageOrderDTOAssembler.java
@@ -0,0 +1,17 @@
+package com.coderpwh.member.application.assembler.domain;
+
+import com.coderpwh.member.application.dto.MemberPackageDTO;
+import com.coderpwh.member.common.util.mapstruct.MapStructConverter;
+import com.coderpwh.member.domain.model.MemberPackage;
+import lombok.Data;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+/**
+ * @author coderpwh
+ * @date 2023/6/7 13:55
+ */
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MemberPackageOrderDTOAssembler extends MapStructConverter<MemberPackageDTO, MemberPackage> {
+
+}
