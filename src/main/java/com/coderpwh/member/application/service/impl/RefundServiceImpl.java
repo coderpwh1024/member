79e97fd5b75c0e7b27502a291e9613fd4aa25de6
coderpwh <pwhfbc@outlook.com>
2023-06-29 16:22:04 +0800

    fix:更新校验

--- src/main/java/com/coderpwh/member/application/service/impl/RefundServiceImpl.java
+++ src/main/java/com/coderpwh/member/application/service/impl/RefundServiceImpl.java
@@ -1,8 +1,10 @@
 package com.coderpwh.member.application.service.impl;
 
 import com.coderpwh.member.application.command.MemberRefundCommand;
+import com.coderpwh.member.application.dto.MemberUserDTO;
 import com.coderpwh.member.application.service.RefundService;
 import com.coderpwh.member.application.vo.MemberRefundVO;
+import com.coderpwh.member.domain.util.LoginUtil;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +18,8 @@ public class RefundServiceImpl implements RefundService {
 
     @Override
     public MemberRefundVO saveRefund(MemberRefundCommand command) {
+        // 获取登录用户
+        MemberUserDTO memberUser = LoginUtil.loginUser();
         return null;
     }
 
--- src/main/java/com/coderpwh/member/domain/specification/RefundSpecification.java
+++ src/main/java/com/coderpwh/member/domain/specification/RefundSpecification.java
@@ -1,17 +1,42 @@
 package com.coderpwh.member.domain.specification;
 
+import com.coderpwh.member.application.command.MemberRefundCommand;
+import com.coderpwh.member.application.dto.MemberUserDTO;
 import com.coderpwh.member.common.ddd.AbstractSpecification;
+import com.coderpwh.member.common.util.enums.DddEnum;
+import com.coderpwh.member.common.util.enums.SysReturnCode;
+import com.coderpwh.member.common.util.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.ObjectUtils;
 
 /**
  * 退款校验
+ *
  * @author coderpwh
  * @date 2023/6/27 17:11
  */
+@Slf4j
 public class RefundSpecification extends AbstractSpecification<Integer> {
 
     @Override
     public boolean isSatisfiedBy(Integer integer) {
         return false;
     }
+
+
+    public boolean isValidRefund(MemberRefundCommand command, MemberUserDTO memberUser) {
+        if (StringUtils.isBlank(command.getPartnerOrderNumber()) && StringUtils.isNotBlank(command.getPartnerOrderNumber())) {
+            log.error("订单号与合作方订单号不能同时为空");
+            throw new BusinessException(SysReturnCode.CarGo, DddEnum.APPLICATIN, "订单号与合作方订单号不能同时为空");
+        }
+
+        if (ObjectUtils.isEmpty(memberUser)) {
+            log.error("当前用户尚未登录");
+            throw new BusinessException(SysReturnCode.CarGo, DddEnum.APPLICATIN, "当前用户尚未登录");
+        }
+        return true;
+    }
+
 
 }
